896926806cf146447c874a4bd20b744e034e821f
raitlahcine-wq <raitlahcine@gmail.com>
2026-07-03 17:22:53 +0100

    Update main.py

--- server/main.py
+++ server/main.py
@@ -26,7 +26,7 @@ app = FastAPI(title="MovieBox Stremio Addon", version="1.0.0", lifespan=lifespan
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
